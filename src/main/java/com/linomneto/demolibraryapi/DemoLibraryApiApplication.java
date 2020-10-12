9316e02d24428ff941c802a412f9a5590b86f725
Lino A. Melhado Neto <linomneto@gmail.com>
2020-10-12 16:19:28 -0300

    first commit: spring initializr

---
+++ src/main/java/com/linomneto/demolibraryapi/DemoLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.linomneto.demolibraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLibraryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linomneto/demolibraryapi/DemoLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linomneto.demolibraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
